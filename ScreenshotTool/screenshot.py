117543c3d4011f5a7e982d644ffa115f560feb87
mocobk <mailmzb@qq.com>
2018-04-28 09:04:30 +0800

    update code

--- ScreenshotTool/screenshot.py
+++ ScreenshotTool/screenshot.py
@@ -2,14 +2,16 @@ import paramiko
 import sys
 import subprocess
 import os
+import threading
 import time
+from flask import Response, Flask
 from globalhotkeys import GlobalHotKeys
 
 
 def progressbar(cur, total):
-    percent = '{:.2%}'.format(cur / total)
-    sys.stdout.write('\r')
-    sys.stdout.write('[%-50s] %s' % ('=' * int(cur / total * 50), percent))
+    sys.stdout.write(' ' * 80 + '\r')
+    sys.stdout.flush()
+    sys.stdout.write('正在下载...\t%sM/%sM\r' % (round(cur / 1048576, 1), round(total / 1048576, 1)))
     sys.stdout.flush()
     if cur == total:
         sys.stdout.write('\n')
@@ -28,6 +30,7 @@ def screenshot(hostname, port, username, password):
 
     trans = ssh.get_transport()  # 从ssh连接中获取transport对象
     sftp = paramiko.SFTPClient.from_transport(trans)
+    # sftp.get('%s/Documents/ios_packages.zip' % remote_dir, 'fb_data.raw', callback=progressbar)
     sftp.get('%s/fb_data.raw' % remote_dir, 'fb_data.raw', callback=progressbar)
     command = 'rm -rf %s/fb_data.raw' % remote_dir
     ssh.exec_command(command)
@@ -35,19 +38,43 @@ def screenshot(hostname, port, username, password):
 
     # windows下转码，并打开截图
     screenshot_name = screenshot_time + '.png'
-    cmd = 'ffmpeg -loglevel quiet -vcodec rawvideo -f rawvideo -pix_fmt rgb565 -s 800X600 -i fb_data2.raw -f image2 ' \
+    cmd = 'ffmpeg -loglevel quiet -vcodec rawvideo -f rawvideo -pix_fmt rgb565 -s 800X600 -i fb_data.raw -f image2 ' \
           '-vcodec png %s' % screenshot_name
     subprocess.Popen(cmd, shell=True).wait()
     cur_path = os.getcwd()
     print(os.path.join(cur_path, screenshot_name))
     subprocess.Popen('start %s' % screenshot_name, shell=True)
+    return screenshot_name
+
+
+def run_screenshot():
+    return screenshot('172.16.7.83', 22, 'tests', 'mpr123')
 
 
 @GlobalHotKeys.register(GlobalHotKeys.VK_F1, GlobalHotKeys.MOD_SHIFT)
+def hot_key_screenshot():
+    run_screenshot()
+
+
+app = Flask(__name__)
+
+
+@app.route('/screenshot')
+def web_screenshot():
+    screenshot_name = run_screenshot()
+    image = open(screenshot_name, mode='rb')
+    return Response(image, mimetype='image/jpeg')
+
+
 def main():
-    screenshot('172.16.7.83', 22, 'tests', 'mpr123')
+    print('请按Shift + F1 开始截图')
+    thread = threading.Thread(target=app.run, args=('0.0.0.0', '5000'))
+    thread.start()
+    GlobalHotKeys.listen()
 
 
 if __name__ == '__main__':
-    print('请按Shift + F1 开始截图')
-    GlobalHotKeys.listen()
+    # print('请按Shift + F1 开始截图')
+    # GlobalHotKeys.listen()
+    # app.run(host='0.0.0.0', port='5000')
+    main()
